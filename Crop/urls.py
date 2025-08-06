511b1bc2e07242b2bbf375f44ccab75b7c5a0ed8
Aditya <adikadia05@gmail.com>
2025-08-06 19:47:18 +0530

    createad a readme.md file

--- Crop/urls.py
+++ Crop/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('', views.login, name='login'),
     path("home/", views.home, name='home'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout, name='logout'),
